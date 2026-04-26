ed539d4a91d580a988f554ce1c730441f19ebe38
HiengLe <lethihien04122001@gmail.com>
2026-04-26 19:42:29 +0700

    Pratice Git PR

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -2,6 +2,7 @@ import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
   await page.goto('https://playwright.dev/');
+  console.log('Title of the page is: ' + await page.title());
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
